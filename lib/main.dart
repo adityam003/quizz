3a7f2e8c339401457a7eccad812703c897f93aff
Aditya <adityamurali003@gmail.com>
2024-03-20 21:47:56 +0530

    restructure

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,24 @@
 
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:file_picker/file_picker.dart';
+import 'dart:io';
+import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:syncfusion_flutter_pdf/pdf.dart';
-import 'package:open_file/open_file.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
 import 'save_file_mobile_and_desktop.dart'
 if (dart.library.html) 'save_file_web.dart';
+
+
 
 void main() {
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -93,13 +101,13 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             TextButton(
               child: Text(
-                'Extract Text from PDF',
+                'Upload',
                 style: TextStyle(color: Colors.white),
               ),
               style: ButtonStyle(
                   backgroundColor: MaterialStateProperty.resolveWith(
                           (states) => Colors.blue)),
-              onPressed: _extractTextFromPDF,
+              onPressed: _generateQuestion,
             )
           ],
         ),
@@ -107,33 +115,85 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  Future<void> _extractTextFromPDF() async{
+  Future<String> _extractTextFromPDF() async{
     final result = await FilePicker.platform.pickFiles(allowMultiple: false);
+
     if(result!= null) {
       print(result.files.first.name);
 
+      final file = File(result.files.first.path!);
+
+      // Read the bytes
+      final bytes = await file.readAsBytes();
+
+      print(file);
+     // file == OpenAppFile.open(file.path.toString());
 
       //Load the PDF document.
-      final PdfDocument document =
-      PdfDocument(inputBytes: await _readDocumentData(result.files.first.name));
-
+      // final PdfDocument document =
+      // PdfDocument(inputBytes: await _readDocumentData(result.files.first.bytes));
+      PdfDocument document = PdfDocument(inputBytes: bytes);
       //Create PDF text extractor to extract text.
+
       PdfTextExtractor extractor = PdfTextExtractor(document);
 
       //Extract text.
       String text = extractor.extractText();
-
+      if(text!= '') {
+        print('text is read');
+      }
+      else
+        print("Nothing is read");
       // Dispose the document.
       document.dispose();
-
+     return text;
       //Save the file and launch/download.
-      SaveFile.saveAndLaunchFile(text, 'output.txt');
+   //   SaveFile.saveAndLaunchFile(text, 'output.txt');
     }
+    else
+      return "unsuccessful";
   }
-
-  Future<List<int>> _readDocumentData(String name) async {
-    final ByteData data = await rootBundle.load('assets/pdf/$name');
-    return data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+Future<void> _generateQuestion() async{
+  // Access your API key as an environment variable (see "Set up your API key" above)
+  const String apiKey = String.fromEnvironment('TMDB_KEY');
+  if (apiKey == '') {
+    print('No \$API_KEY environment variable');
+    exit(1);
   }
+  // For text-only input, use the gemini-pro model
+  final model = GenerativeModel(model: 'gemini-pro', apiKey: apiKey);
+  const String prompt = '''from the given input create 5 questions in json and give 4 mcq type options for each question and answer in json(strictly follow the pattern and no extra word or character and start with square bracket directly no back ticks) following is the pattern
+  [{
+    "question": "Which of the following is a major river in India?",
+    "options": [
+      "Ganges",
+      "Nile",
+      "Amazon",
+      "Mississippi"
+    ],
+    "answer": "Ganges"
+  },
+{
+  "question": "Which city is the capital of India?",
+  "options": [
+  "Mumbai",
+  "New Delhi",
+  "Kolkata",
+  "Chennai"
+  ],
+  "answer": "New Delhi"
+  },
+  ]''';
+  String input =  await _extractTextFromPDF();
+  final content = [Content.text(prompt + input)];
+  final response = await model.generateContent(content);
+  final result = response.text;
+  final obj = jsonEncode(result);
+  final questions= jsonDecode(obj);
+  // Arrays to store questions and options
+  print(questions);
+  //print('Question : ${questions['questions']}');
 
+  // print(response.text);
 }
+}g
